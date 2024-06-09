9cf676fa6efd3f33b58f18e49773f92bbf275341
Alexia <Elaina-Alex@proton.me>
2024-06-09 21:39:12 +0800

    ~

--- app.go
+++ app.go
@@ -10,17 +10,33 @@ import (
 type APP struct {
 	threadNum int
 	client    *Client
+	bookInfo  *boluobaomodel.BookInfoData
 }
 
 func (client *Client) APP() *APP {
 	return &APP{client: client, threadNum: 32}
 }
+
+func (app *APP) MergeText(f func(chapter boluobaomodel.ChapterList)) {
+	if app.bookInfo == nil {
+		fmt.Println("Please set book info first!")
+		return
+	}
+	app.eachChapter(f)
+}
+
 func (app *APP) SetThreadNum(threadNum int) *APP {
 	app.threadNum = threadNum
 	return app
 }
-func (app *APP) EachChapter(bookInfo *boluobaomodel.BookInfoData, f func(boluobaomodel.ChapterList)) {
-	divisionList, err := app.client.API().GetCatalogue(bookInfo.NovelId)
+
+func (app *APP) SetBookInfo(bookInfo *boluobaomodel.BookInfoData) *APP {
+	app.bookInfo = bookInfo
+	return app
+}
+
+func (app *APP) eachChapter(f func(boluobaomodel.ChapterList)) {
+	divisionList, err := app.client.API().GetCatalogue(app.bookInfo.NovelId)
 	if err != nil {
 		fmt.Println("get division list error:", err)
 		return
@@ -32,41 +48,49 @@ func (app *APP) EachChapter(bookInfo *boluobaomodel.BookInfoData, f func(boluoba
 	}
 }
 
-func (app *APP) Download(bookInfo *boluobaomodel.BookInfoData, f1 continueFunction, f2 contentFunction) {
+func (app *APP) Download(f1 continueFunction, f2 contentFunction) {
+	if app.bookInfo == nil {
+		fmt.Println("Please set book info first!")
+		return
+	}
+
 	var wg sync.WaitGroup
 	ch := make(chan struct{}, app.threadNum)
-	app.EachChapter(bookInfo, func(chapter boluobaomodel.ChapterList) {
+
+	app.eachChapter(func(chapter boluobaomodel.ChapterList) {
 		wg.Add(1)
 		ch <- struct{}{}
+
 		go func(chapter boluobaomodel.ChapterList) {
 			defer func() {
 				wg.Done()
 				<-ch
 			}()
-			if f1(bookInfo, chapter) {
+			if f1(app.bookInfo, chapter) {
 				content, err := app.client.API().GetChapterContent(chapter.ChapID)
 				if err != nil {
 					fmt.Println("get chapter content error:", err)
 					return
 				}
-				f2(bookInfo, &content.Data)
+				f2(app.bookInfo, &content.Data)
 			}
 		}(chapter)
 	})
+
 	wg.Wait()
 }
 
 func (app *APP) Search(keyword string, f1 continueFunction, f2 contentFunction) {
 	searchInfo, err := app.client.API().GetSearch(keyword, 0)
 	if err != nil {
-		fmt.Println("search failed!" + err.Error())
+		fmt.Println("search failed! " + err.Error())
 		return
 	}
 	searchInfo.EachBook(func(index int, book boluobaomodel.BookInfoData) {
-		fmt.Println("Index:", index, "\t\t\tBookName:", book.NovelName)
+		fmt.Println("Index:", index, "\tBookName:", book.NovelName)
 	})
-	bookInfo := searchInfo.GetBook(input.IntInput("Please input the index of the book you want to download"))
-	app.Download(bookInfo, f1, f2)
+	app.bookInfo = searchInfo.GetBook(input.IntInput("Please input the index of the book you want to download"))
+	app.Download(f1, f2)
 }
 
 func (app *APP) Bookshelf(f1 continueFunction, f2 contentFunction) {
@@ -76,13 +100,12 @@ func (app *APP) Bookshelf(f1 continueFunction, f2 contentFunction) {
 		return
 	}
 	shelf.EachShelf(func(index int, shelf boluobaomodel.ShelfData) {
-		fmt.Println("Index:", index, "\t\t\tShelfName:", shelf.Name, "\t\t\tShelfNum:", len(shelf.Expand.Novels))
+		fmt.Println("Index:", index, "\tShelfName:", shelf.Name, "\tShelfNum:", len(shelf.Expand.Novels))
 	})
 	bookshelf := shelf.GetShelf(input.IntInput("Please input the index of the bookshelf you want to download"))
 	bookshelf.EachBookshelf(func(index int, book boluobaomodel.BookInfoData) {
-		fmt.Println("Index:", index, "\t\t\tBookName:", book.NovelName)
+		fmt.Println("Index:", index, "\tBookName:", book.NovelName)
 	})
-	bookInfo := bookshelf.GetBookshelf(input.IntInput("Please input the index of the book you want to download"))
-	app.Download(bookInfo, f1, f2)
-
+	app.bookInfo = bookshelf.GetBookshelf(input.IntInput("Please input the index of the book you want to download"))
+	app.Download(f1, f2)
 }
